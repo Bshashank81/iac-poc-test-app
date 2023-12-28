bc4a6f1e075f85b1cb566e89c5e3ac38dc3d5d1c
bshashank-81 <shashank.balabhadra@zemosolabs.com>
2023-12-28 15:58:33 +0530

    Fix issues in the ngnix controller

--- eks/providers.tf
+++ eks/providers.tf
@@ -12,3 +12,10 @@ provider "kubectl" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.for_token.token
 }
+
+provider "aws" {
+  region = "us-east-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
